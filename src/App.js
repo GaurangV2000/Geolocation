3724698fc565b05513e4cd85e1986409a5ed0681
GaurangV2000 <gaurangvasoya1234@gmail.com>
2022-09-14 17:41:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countryName: '',
+      countryCode: ''
+    }
+  }
+  getGeoInfo = () => {
+    axios.get('https://ipapi.co/json/').then((response) => {
+      let data = response.data;
+      console.log(response)
+      this.setState({
+        countryName: data.country_name,
+        countryCode: data.country_calling_code
+      });
+    }).catch((error) => {
+      console.log(error);
+    });
+  };
+  componentDidMount() {
+    this.getGeoInfo();
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Country Name: {this.state.countryName}</p>
+        <p>Country Code: {this.state.countryCode}</p>
+      </div>
+    )
+  }
 }
-
-export default App;
